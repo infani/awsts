47cbeedb72f522958886dac27ba9dd560b189bf7
shadow.lu <a520758758@gmail.com>
2022-07-19 15:22:04 +0800

    finx yarn lint error

--- src/appsync/index.test.ts
+++ src/appsync/index.test.ts
@@ -3,6 +3,6 @@ import { AppSync } from './index';
 describe('index', () => {
   it('index', async () => {
     const res = await AppSync.GetApiKey('vtib2crdejecxibzdt5rlklw44');
-    expect(res).not.toBeUndefined()
+    expect(res).not.toBeUndefined();
   });
 });
--- src/appsync/index.ts
+++ src/appsync/index.ts
@@ -1,19 +1,19 @@
-import { AppSyncClient, ListApiKeysCommand } from "@aws-sdk/client-appsync"
+import { AppSyncClient, ListApiKeysCommand } from '@aws-sdk/client-appsync';
 
-const GetApiKey = async(apiId:string)=>{
-    const client = new AppSyncClient({})
-    const command = new ListApiKeysCommand({apiId})
-    const response = await client.send(command)
-    if(!!response.apiKeys){
-        for(let i=0 ; i < response.apiKeys.length;i++){
-            let expires = response.apiKeys[i].expires
-            if (!!expires && expires > (Date.now()/1000)){
-                if(!!response.apiKeys[i].id){
-                    return response.apiKeys[i].id
-                }
-            }
+const GetApiKey = async (apiId: string) => {
+  const client = new AppSyncClient({});
+  const command = new ListApiKeysCommand({ apiId });
+  const response = await client.send(command);
+  if (!!response.apiKeys) {
+    for (let i = 0; i < response.apiKeys.length; i++) {
+      let expires = response.apiKeys[i].expires;
+      if (!!expires && expires > Date.now() / 1000) {
+        if (!!response.apiKeys[i].id) {
+          return response.apiKeys[i].id;
         }
+      }
     }
-    return undefined
-}
+  }
+  return undefined;
+};
 export const AppSync = { GetApiKey };
