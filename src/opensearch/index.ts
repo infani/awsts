7da7f437f5e14df473ccbc5ed9adab8722689c4d
Ben <ben@vivotek.com>
2022-07-27 01:41:02 +0000

    add create

--- src/opensearch/index.ts
+++ src/opensearch/index.ts
@@ -3,9 +3,16 @@ import { Client } from '@opensearch-project/opensearch'
 const NewClient = (url: string, user: string, password: string) => {
     const client = new Client({
         node: `https://${user}@${password}@${url}:9200`,
-      });
+    });
+    const Create = async (indexName: string, doc: any) => {
+        const res = await client.index({
+            index: indexName,
+            body: doc,
+        });
+        console.log(res.body);
+    }
     return {
-        client,
+        Create
     }
 }
 
