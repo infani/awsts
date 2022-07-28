f6608990bcaa1548fae15aedcfa806520869292b
Ben <ben@vivotek.com>
2022-07-28 00:38:26 +0000

    fix auth

--- src/opensearch/index.ts
+++ src/opensearch/index.ts
@@ -2,7 +2,11 @@ import { Client } from '@opensearch-project/opensearch'
 
 const NewClient = (url: string, user: string, password: string) => {
     const client = new Client({
-        node: `https://${user}:${password}@${url}`,
+        node: `https://${url}`,
+        auth: {
+            username: user,
+            password,
+        }
     });
     const Create = (indexName: string, doc: any) => {
         const res = client.index({
