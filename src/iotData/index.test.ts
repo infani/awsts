9c8798a9208460641b38a72d649a1ec62895b5ae
Ben <ben@vivotek.com>
2022-09-19 13:38:04 +0000

    add Client to iotData

--- src/iotData/index.test.ts
+++ src/iotData/index.test.ts
@@ -1,8 +1,19 @@
 import { IotData } from './index';
 
 describe('index', () => {
-  it('index', async () => {
+  it('Publish', async () => {
     const res = await IotData.Publish('test', "test");
     expect(res.$metadata.httpStatusCode).toMatchSnapshot();
+  });
+
+  it('Client', async () => {
+    const cli = await IotData.Client(
+      './src/iotData/certificate.pem.crt',
+      './src/iotData/private.pem.key',
+      './src/iotData/AmazonRootCA1.pem',
+      'a1whrgfehabj9-ats.iot.ap-northeast-1.amazonaws.com'
+    );
+    // await cli.connect()
+    cli.publish("test", 'test2', 0)
   });
 });
--- src/iotData/index.ts
+++ src/iotData/index.ts
@@ -3,6 +3,9 @@ import {
   PublishCommand,
 } from '@aws-sdk/client-iot-data-plane';
 
+import { mqtt } from 'aws-iot-device-sdk-v2';
+import { iot } from 'aws-iot-device-sdk-v2';
+
 const Publish = async (topic: string, msg: string) => {
   const client = new IoTDataPlaneClient({});
   const command = new PublishCommand({
@@ -13,4 +16,17 @@ const Publish = async (topic: string, msg: string) => {
   return res;
 };
 
-export const IotData = { Publish };
+const Client = async (certFile: string, keyFile: string, caFile: string, endpoint: string) => {
+  let config_builder = iot.AwsIotMqttConnectionConfigBuilder.new_mtls_builder_from_path(certFile, keyFile);
+  config_builder.with_certificate_authority_from_path(undefined, caFile);
+
+  config_builder.with_clean_session(false);
+  config_builder.with_client_id("test-" + Math.floor(Math.random() * 100000000));
+  config_builder.with_endpoint(endpoint);
+  const config = config_builder.build();
+
+  const client = new mqtt.MqttClient();
+  return client.new_connection(config);
+}
+
+export const IotData = { Publish, Client };
