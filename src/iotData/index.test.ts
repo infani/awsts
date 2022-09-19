77c866475a5f615f70726fba4e046e308960d7b8
Ben <ben@vivotek.com>
2022-09-19 08:23:26 +0000

    rm

--- src/iotData/index.test.ts
+++ src/iotData/index.test.ts
@@ -1,6 +1,5 @@
 import { IotData } from './index';
 
-//AWS_PROFILE=default yarn test src/iotData/index.test.ts
 describe('index', () => {
   it('index', async () => {
     const res = await IotData.Publish('test', "test");
